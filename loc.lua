5463c3c50836138e40e5d3392c2e56d19ae78f89
Rhynne <douglaskmarx@gmail.com>
2023-03-28 04:47:02 -0600

    Update loc.lua

--- loc.lua
+++ loc.lua
@@ -1,7 +1,10 @@
 
 Hooks:Add("LocalizationManagerPostInit", "Remix_Localization", function(loc)
 	LocalizationManager:add_localized_strings({
-		["heist_firestarter_remix"] = "Firestarter Remixed",
+		["heist_firestarter_remix"] = "Firestarter: Remixed",
+		["heist_alex_remix"] = "Rats: Remixed",
+		["heist_welcome_to_the_jungle_remix"] = "Big Oil: Remixed",
+		["heist_ukrainian_job_remix"] = "Ukrainian Job: Remixed"
 	})
 end)
 
