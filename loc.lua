dcb2f67fb3c72971e3dfda16acc4b3fec76eb40f
Rhynne <douglaskmarx@gmail.com>
2023-04-07 13:04:45 -0600

    four stores remixed art test

--- loc.lua
+++ loc.lua
@@ -4,6 +4,7 @@ Hooks:Add("LocalizationManagerPostInit", "Remix_Localization", function(loc)
 		["heist_firestarter_remix"] = "Firestarter: Remixed",
 		["heist_alex_remix"] = "Rats: Remixed",
 		["heist_welcome_to_the_jungle_remix"] = "Big Oil: Remixed",
+		["heist_four_stores_remixed"] = "Four Stores: Remixed",
 		["heist_ukrainian_job_remix"] = "Ukrainian Job: Remixed"
 	})
 end)
