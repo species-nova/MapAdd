aa04fc150b3446d810bf595500dd02724da2e9b7
Rhynne <douglaskmarx@gmail.com>
2024-10-01 18:55:04 -0400

    undercover, four stores changes

--- loc.lua
+++ loc.lua
@@ -1,7 +1,7 @@
 
 Hooks:Add("LocalizationManagerPostInit", "Remix_Localization", function(loc)
 	LocalizationManager:add_localized_strings({
-		-- heist names
+		-- retail
 		["heist_firestarter_remix"] = "Firestarter: Remixed",
 		["heist_alex_remix"] = "Rats: Remixed",
 		["heist_welcome_to_the_jungle_remix"] = "Big Oil: Remixed",
@@ -13,12 +13,17 @@ Hooks:Add("LocalizationManagerPostInit", "Remix_Localization", function(loc)
 		["heist_framing_frame_remix_name"] = "Framing Frame: Remixed",
 		["heist_mallcrasher_remix_name"] = "Mallcrasher: Remixed",
 		
+		-- classics
+		["heist_secret_stash_name"] = "Undercover: Remixed",
+		["heist_green_bridge_name"] = "Green Bridge: Remixed",
+		
+		-- custom/recreations
 		["heist_greenharvest_name"] = "Green Harvest: Remixed", --instead of making BB remixed, aim for this
 		["heist_great_train_remix_name"] = "Time Window: Remixed", --early planning
 		["heist_easy_store_remix_name"] = "Twenty-Four-Seven: Remixed", --teehee
 		
 		
-		--assets
+		-- assets
 		["menu_alex_1_zipline"] = "Bag Zipline",
 		["menu_alex_1_zipline_desc"] = "Pay Hector's cooks for a bag zipline back to the van area.",
 		
@@ -26,7 +31,7 @@ Hooks:Add("LocalizationManagerPostInit", "Remix_Localization", function(loc)
 		
 		
 		
-		--subtitles for audio
+		-- subtitles for audio
 		["mapadd_ukr_01"] = "Don't screw around in there - get it done!",
 		["mapadd_ukr_02"] = "Halfway mark... hurry up!",
 		["mapadd_ukr_03"] = "Got a minute 'till the van leaves - with or without 'ya!",
